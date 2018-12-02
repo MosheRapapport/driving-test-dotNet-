b44b55a2ee5ccd388a89b9c6e443bad350b058b3
Yoni <yonilabell@gmail.com>
2018-12-02 16:16:02 +0200

    start

---
+++ BE/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public struct Address
+    {
+        public String StreetName { get; set; }
+        public int Number { get; set; }
+        public String City { get; set; }
+
+    }
+}
---
+++ BE/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Person
+    {
+        public String ID { get; set; }
+        public Name Name { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public Address Address { get; set; }
+
+    }
+}
